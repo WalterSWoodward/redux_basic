0fb31da4e4f2e05d7e7a119232045282a1433560
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-07-04 23:45:34 -0400

    Adds Container Components fixes a few small bugs as well

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-​
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const Todo = ({ onClick, completed, text }) => (
   <li
     onClick={onClick}
-    style={ {
+    style={{
       textDecoration: completed ? 'line-through' : 'none'
     }}
   >
     {text}
   </li>
-)
-​
+);
+
 // PROPTYPES!!! :O
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
-}
-​
-export default Todo
+};
+
+export default Todo;
